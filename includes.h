d21ea38aad5aaf2492b383fe2ccc9c143d2123b4
byshen <ahshenbingyu@163.com>
2015-11-05 21:24:40 +0800

    lex finished

--- includes.h
+++ includes.h
@@ -1,12 +1,12 @@
 #ifndef INCLUDES
 #define INCLUDES
 
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <map>
+//#include <iostream>
+//#include <fstream>
+//#include <string>
+//#include <map>
 #include <stdio.h>
 #include <stdlib.h> 
-using namespace std;
+//using namespace std;
 
 #endif //INCLUDES
