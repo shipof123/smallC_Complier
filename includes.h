a46275d86a1487ac51cd42d137abbc590afcd163
byshen <ahshenbingyu@163.com>
2015-11-05 20:24:13 +0800

    fixed bugs

---
+++ includes.h
@@ -0,0 +1,12 @@
+#ifndef INCLUDES
+#define INCLUDES
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+#include <stdio.h>
+#include <stdlib.h> 
+using namespace std;
+
+#endif //INCLUDES
