ea9026173f00ea9e6b185acbb7ccadfaba9fe294
byshen <ahshenbingyu@163.com>
2015-11-12 18:02:30 +0800

    new yacc

--- node.h
+++ node.h
@@ -2,39 +2,60 @@
 #define NODE_H
 
 typedef enum {
-	PROGRAM,
-	EXTDEFS,
-	EXTDEF,
-	EXTVARS,
-	SPEC,
-	STSPEC,
-	OPTTAG,
-	VAR,
-	FUNC,
-	PARAS,
-	PARASF,
-	PARA,
-	STMTBLOCK,
-	STMT,
-	ESTMT,
-	DEFS,
-	DEF,
-	DECS,
-	DEC,
-	INIT,
-	FEXP,
-	EXP,
-	ARRS,
-	ARGS
+	Program,
+	Extdefs,
+	Extdef,
+	Extvars,
+	Spec,
+	Stspec,
+	Opttag,
+	Var,
+	Func,
+	Paras,
+	Parasf,
+	Para,
+	Stmtblock,
+	Stmt,
+	Estmt,
+	Defs,
+	Def,
+	Decs,
+	Dec,
+	Init,
+	Fexp,
+	Exp,
+	Arrs,
+	Args,
+	Null,
+	UnaryOp,
+	Operator,
+	Int,
+	Type,
+	Id,
+	Keyword
 } NodeType;
 
 struct Node {
 	int		line_num;
-	string		name;
+	char*		name;
 	int 		size;
 	int 		capacity;	
 	NodeType 	node_type;
 	Node**		children;
+	Node(int lineno, NodeType nt, char* n, int s, ...) {
+		line_num = lineno;
+		node_type = nt;
+		name = strdup(n);
+		size = capacity = s;
+		children = new Node* (s);
+		
+		va_list vl;
+		va_start(vl,s);
+		for (int i = 0; i<s; ++i) {
+			children[i] = va_arg(vl, Node*);
+		}
+		va_end(vl);
+	}		
 };
 
 #endif //NODE_H
